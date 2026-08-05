03af6f2a8e41387c3d176a6c0a677d29bd20a430
Esom-N <esom.seraphim@gmail.com>
2026-08-05 19:34:05 -0400

    Create tables for songs, listening history, and calendar
    Added new tables for listening history, calendar, and staging, along with comments for clarity.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,5 +1,9 @@
 USE spotify_analytics;
 
+-- =====================================
+-- SONG TABLE
+-- Stores unique song information
+-- =====================================
 
 CREATE TABLE songs (
     song_id INT PRIMARY KEY,
@@ -9,17 +13,29 @@ CREATE TABLE songs (
 );
 
 
+-- =====================================
+-- LISTENING HISTORY TABLE
+-- Stores individual Spotify listening events
+-- =====================================
+
 CREATE TABLE listening_history (
     listening_id INT PRIMARY KEY,
     song_id INT,
-    date DATE,
+    timestamp DATETIME,
     minutes_played DECIMAL(5,2),
     platform VARCHAR(100),
-    
+    skipped BOOLEAN,
+    shuffle BOOLEAN,
+
     FOREIGN KEY (song_id)
     REFERENCES songs(song_id)
 );
 
+
+-- =====================================
+-- CALENDAR TABLE
+-- Stores date attributes for time analysis
+-- =====================================
 
 CREATE TABLE calendar (
     date DATE PRIMARY KEY,
@@ -27,4 +43,25 @@ CREATE TABLE calendar (
     month VARCHAR(20),
     season VARCHAR(20),
     semester VARCHAR(20)
+);
+
+
+-- =====================================
+-- STAGING TABLE
+-- Temporary table used for loading cleaned Spotify data
+-- =====================================
+
+CREATE TABLE spotify_staging (
+    timestamp DATETIME,
+    platform VARCHAR(100),
+    ms_played INT,
+    song_name VARCHAR(255),
+    artist_name VARCHAR(255),
+    album_name VARCHAR(255),
+    shuffle BOOLEAN,
+    skipped BOOLEAN,
+    minutes_played DECIMAL(5,2),
+    year INT,
+    month VARCHAR(20),
+    season VARCHAR(20)
 );
