d9e34572febfb6c3b999075289dd886ffee12e1c
Esom-N <esom.seraphim@gmail.com>
2026-08-05 19:34:41 -0400

    Update create_database.sql

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,3 +1,9 @@
+-- =====================================
+-- CREATE DATABASE
+-- Spotify Behavioral Analytics Project
+-- =====================================
+
 CREATE DATABASE spotify_analytics;
 
+-- Select database for use
 USE spotify_analytics;
